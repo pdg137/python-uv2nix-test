d66d8a4cf3f66e6708531b081f3b28a03450affb
Paul Grayson <pdg@alum.mit.edu>
2025-06-12 17:48:17 -0700

    add openssl to LD_LIBRARY_PATH so QNetworkAccessManager will work

--- default.nix
+++ default.nix
@@ -87,7 +87,10 @@ in
     env =  {
       UV_PYTHON_DOWNLOADS = "never";
       UV_PYTHON = python.interpreter;
-      LD_LIBRARY_PATH = lib.makeLibraryPath pkgs.pythonManylinuxPackages.manylinux1;
+      LD_LIBRARY_PATH = lib.makeLibraryPath (
+        pkgs.pythonManylinuxPackages.manylinux1 ++ [
+          pkgs.openssl # required for https requests (QNetworkAccessManager)
+        ]);
     };
 
     # Prevent nixpkgs from being garbage-collected when building a
